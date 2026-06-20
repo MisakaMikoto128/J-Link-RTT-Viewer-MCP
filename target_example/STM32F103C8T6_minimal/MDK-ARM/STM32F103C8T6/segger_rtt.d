8b14ea277c0ab56fc4fc8e5291c826ec370d9c89
liuyuanlin <5270497+liuyuanlin6445@user.noreply.gitee.com>
2026-06-20 18:17:08 +0800

    清理工作区：更新gitignore忽略Keil编译产物
    忽略规则：
    - MDK-ARM/STM32F103C8T6/ (编译输出目录)
    - *.uvguix.*, *.uvoptx (Keil用户配置)
    - JLinkLog.txt, JLinkSettings.ini
    - *.axf, *.hex, *.map, *.htm, *.o, *.d
    - RTE/, DebugConfig/, EventRecorderStub.scvd

--- target_example/STM32F103C8T6_minimal/MDK-ARM/STM32F103C8T6/segger_rtt.d
+++
@@ -1,3 +0,0 @@
-stm32f103c8t6/segger_rtt.o: ..\third\ party\SEGGER\SEGGER_RTT.c \
-  ..\third\ party\SEGGER\SEGGER_RTT.h \
-  ..\third\ party\SEGGER\SEGGER_RTT_Conf.h
--- target_example/STM32F103C8T6_minimal/MDK-ARM/STM32F103C8T6/segger_rtt_printf.d
+++
@@ -1,4 +0,0 @@
-stm32f103c8t6/segger_rtt_printf.o: \
-  ..\third\ party\SEGGER\SEGGER_RTT_printf.c \
-  ..\third\ party\SEGGER\SEGGER_RTT.h \
-  ..\third\ party\SEGGER\SEGGER_RTT_Conf.h
